1d35ee3ee122b00c9baa28630d1a68ad42b6521b
Tonypadgett <157521742+Tonypadgett@users.noreply.github.com>
2024-03-08 09:42:47 -0500

    Update not tag.cs

--- not tag.cs
+++ not tag.cs
@@ -1,1 +1,37 @@
 
+using easyInputs;
+using GorillaLocomotion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Maximility.Menu.Mods.Movement
+{
+    public class not tagMods
+    {
+        public static void not tag()
+        {
+            if (EasyInputs.GetPrimaryButtonDown(EasyHand.RightHand))
+            {
+                GorillaLocomotion.Player.Instance.bodyCollider.attachedRigidbody.velocity += GorillaLocomotion.Player.Instance.rightHandTransform.right / 2f;
+            }
+
+            if (EasyInputs.GetPrimaryButtonDown(EasyHand.LeftHand))
+            {
+                GorillaLocomotion.Player.Instance.bodyCollider.attachedRigidbody.velocity += -GorillaLocomotion.Player.Instance.leftHandTransform.right / 2f;
+            }
+        }
+
+        public static void not tag()
+        {
+            if (EasyInputs.GetTriggerButtonDown(EasyHand.LeftHand))
+            {
+                GorillaLocomotion.Player.Instance.transform.position += GorillaLocomotion.Player.Instance.leftHandTransform.transform.forward * 0.3f;
+                GorillaLocomotion.Player.Instance.bodyCollider.attachedRigidbody.velocity = Vector3.zero;
+            }
+        }
+    }
+}
